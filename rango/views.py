cdf56183ddf37f70bc7426074864248cb086fb33
Cheuk Yeung Sophia Chung <2572284c@student.gla.ac.uk>
2021-02-11 21:50:28 +0800

    ch10 final commit

--- rango/views.py
+++ rango/views.py
@@ -12,7 +12,11 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 def index(request):
-    # Query database for all categories -> order by likes
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
@@ -23,33 +27,36 @@ def index(request):
 
     # Call the helper function to handle the cookies
     visitor_cookie_handler(request)
-    
-    # Obtain our Response object early so we can add cookie information
+
+    # Obtain our Response object early so we can add cookie information.
     response = render(request, 'rango/index.html', context=context_dict)
+    # Return response back to the user, updating any cookies that need changed.
     return response
 
 
 def about(request):
     context_dict = {}
+    visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
 
-    visitor_cookie_handler(request)
-        
-    return render(request, 'rango/about.html', context = context_dict)
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
-    # Create a context dictionary
+    # Create a context dictionary which we can pass
+    # to the template rendering engine.
     context_dict = {}
 
     try:
-        # If we can't find category name slug -> raises a DoesNotExist exception.
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
         # The .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
 
-        # Retrieve all of the associated pages -> filter
+        # Retrieve all of the associated pages.
+        # The filter() will return a list of page objects or an empty list.
         pages = Page.objects.filter(category=category)
 
-        # Adds our results list to the template
+        # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
         
         # We also add the category object from
@@ -136,6 +143,7 @@ def register(request):
         user_form = UserForm(request.POST)
         profile_form = UserProfileForm(request.POST)
 
+        # If the two forms are valid...
         if user_form.is_valid() and profile_form.is_valid():
             # Save the user's form data to the database.
             user = user_form.save()
@@ -182,6 +190,7 @@ def register(request):
                              'registered': registered})
 
 def user_login(request):
+    # If the request is a HTTP POST, try to pull out the relevant information.
     if request.method == 'POST':
         # Gather the username and password provided by the user.
         # This information is obtained from the login form.
@@ -250,22 +259,23 @@ def get_server_side_cookie(request, cookie, default_val=None):
         val = default_val
     return val
 
-    # Updated the function definition
-    def visitor_cookie_handler(request):
-        visits = int(get_server_side_cookie(request, 'visits', '1'))
-        last_visit_cookie = get_server_side_cookie(request,
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
                                                'last_visit',
                                                str(datetime.now()))
-        last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                             '%Y-%m-%d %H:%M:%S')
 
-        if (datetime.now() - last_visit_time).days > 0:
-            visits = visits + 1
-            # Update the last visit cookie now that we have updated the count
-            request.session['last_visit'] = str(datetime.now())
-        else:
-            # Set the last visit cookie
-            request.session['last_visit'] = last_visit_cookie
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
 
-        # Update/set the visits cookie
-        request.session['visits'] = visits
+    # Update/set the visits cookie
+    request.session['visits'] = visits
